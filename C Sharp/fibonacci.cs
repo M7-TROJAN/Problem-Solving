7285bb4ace62e8d9088ff28bfc7096bb196959e5
Mahmoud Mohamed <matterm74m7@gmail.com>
2023-11-15 18:29:02 +0200

    Update fibonacci.cs

--- C Sharp/fibonacci.cs
+++ C Sharp/fibonacci.cs
@@ -12,7 +12,7 @@ internal class Program
             // Fibonacci [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
             for (int i = 0, prev = 0, current = 1; i < counter; ++i)
             {
-                Console.WriteLine(prev);
+                Console.Write(prev + " ");
                 int newFib = prev + current;
                 prev = current;
                 current = newFib;
