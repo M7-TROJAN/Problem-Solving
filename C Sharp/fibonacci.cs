77f57d38f22ba0236dc60a27e5baa80e2d223856
Mahmoud Mohamed <matterm74m7@gmail.com>
2023-11-15 18:27:32 +0200

    Add files via upload

---
+++ C Sharp/fibonacci.cs
@@ -0,0 +1,28 @@
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of Fibonacci numbers to generate: ");
+
+        // Read user input and parse it to get the counter value
+        if (int.TryParse(Console.ReadLine(), out int counter) && counter > 0)
+        {
+            // Fibonacci [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+            for (int i = 0, prev = 0, current = 1; i < counter; ++i)
+            {
+                Console.WriteLine(prev);
+                int newFib = prev + current;
+                prev = current;
+                current = newFib;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid positive integer.");
+        }
+
+        Console.ReadKey();
+    }
+}
