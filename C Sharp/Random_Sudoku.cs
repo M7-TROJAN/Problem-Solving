e0cc5e7e563bf2eae368b7e734f930c989f18606
Mahmoud Mohamed <matterm74m7@gmail.com>
2023-11-17 01:15:56 +0200

    Create Random_Sudoku.cs

---
+++ C Sharp/Random_Sudoku.cs
@@ -0,0 +1,75 @@
+using System;
+
+public class Sudoku
+{
+    private readonly int[,] board;
+    private readonly Random random;
+
+    public Sudoku()
+    {
+        // Initialize the Sudoku board and the Random object
+        board = new int[9, 9];
+        random = new Random();
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        // Generate a random Sudoku board
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                // Generate a random number between 1 and 9
+                int randomNumber = random.Next(1, 10);
+                board[i, j] = randomNumber;
+            }
+        }
+    }
+
+    // Indexer for accessing and modifying Sudoku board values
+    public int this[uint row, uint col]
+    {
+        get
+        {
+            if (row < 9 && col < 9)
+                return board[row, col];
+            else
+                throw new ArgumentOutOfRangeException("Invalid matrix indices.");
+        }
+        set
+        {
+            if (row < 9 && col < 9)
+                board[row, col] = value;
+            else
+                throw new ArgumentOutOfRangeException("Invalid matrix indices.");
+        }
+    }
+
+    // Print the Sudoku board
+    public void PrintBoard()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                Console.Write($"{board[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Sudoku sudoku = new Sudoku();
+
+        sudoku.PrintBoard();
+
+
+        Console.WriteLine($"\nValue at index 4, 4: {sudoku[4, 4]}");
+    }
+}
